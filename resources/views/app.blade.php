dcfb1012a214242691b7561a215bac0e1efa3313
tenesh <teneshdev@gmail.com>
2024-12-29 05:11:06 +0800

    Add svelte, typescript and frontend tools

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'netbalance') }}</title>
+        <title inertia>{{ config('app.name', 'netbalance') }}</title>
 
-    @routes
-    @viteReactRefresh
-    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-    @inertiaHead
-</head>
-<body>
-@inertia
-</body>
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.svelte"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.ts',
             refresh: true,
         }),
+        svelte(),
     ],
 });
