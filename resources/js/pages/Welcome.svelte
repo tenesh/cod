f62b6217ab6e19195d9bae7dad3644fa6c0c7d34
tenesh <teneshdev@gmail.com>
2024-12-30 04:39:35 +0800

    Add pages

---
+++ resources/js/pages/Welcome.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import MainLayout from '@/layouts/MainLayout.svelte';
+</script>
+
+<svelte:head>
+    <title>Welcome</title>
+</svelte:head>
+
+<MainLayout>
+    <div></div>
+</MainLayout>
