d516894660c831887f3d6fc896209969a4bd09e5
tenesh <teneshdev@gmail.com>
2025-03-23 08:40:13 +0800

    Update global.rs

--- src-tauri/src/global.rs
+++ src-tauri/src/global.rs
@@ -3,7 +3,7 @@ use std::{fs, path};
 pub fn get_app_config_dir_path() -> path::PathBuf {
     let config_dir = dirs::config_dir().unwrap();
 
-    let app_config_dir = config_dir.join("netbalance");
+    let app_config_dir = config_dir.join("cod");
 
     if !app_config_dir.exists() {
         fs::create_dir(&app_config_dir).unwrap();
@@ -15,7 +15,7 @@ pub fn get_app_config_dir_path() -> path::PathBuf {
 pub fn get_app_db_file_path() -> path::PathBuf {
     let app_config_dir = get_app_config_dir_path();
 
-    let file = app_config_dir.join("netbalance.sqlite3");
+    let file = app_config_dir.join("cod.sqlite3");
 
     if !file.exists() {
         fs::File::create(&file).unwrap();
@@ -27,7 +27,7 @@ pub fn get_app_db_file_path() -> path::PathBuf {
 pub fn get_app_log_file_path() -> path::PathBuf {
     let app_config_dir = get_app_config_dir_path();
 
-    let file = app_config_dir.join("netbalance.log");
+    let file = app_config_dir.join("cod.log");
 
     if !file.exists() {
         fs::File::create(&file).unwrap();
